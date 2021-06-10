b694e13b0746044bb4c24e29c0e7b750a535088b
Equipo <abesoky@hotmail.com>
2021-06-10 20:24:34 -0300

    Cambie el principal

--- TrabajoPractico/src/negocio/TrabajoPractico.java
+++ TrabajoPractico/src/negocio/TrabajoPractico.java
@@ -17,7 +17,7 @@ public class TrabajoPractico {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("HOLA MUNDO");
     }
     
 }
